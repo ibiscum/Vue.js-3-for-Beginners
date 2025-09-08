f3a560318a23116085db5a468faff278a9d64d87
ibiscum <ulf.luecking@gmail.com>
2025-09-08 13:26:31 +0200

    update

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -5,7 +5,7 @@ import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(HelloWorld, { props: { msg: 'You’ve successfully created a project with Vite + Vue 3.' } })
+    expect(wrapper.text()).toContain('You’ve successfully created a project with Vite + Vue 3.')
   })
 })
