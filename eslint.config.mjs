f769ce718e52d918c75e0c5eb11e8ec26fed22c6
ibiscum <ulf.luecking@gmail.com>
2025-11-08 10:37:31 +0100

    ch04

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,21 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
+import pluginCypress from 'eslint-plugin-cypress'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,vue}", "cypress/**/*.js"], 
+    plugins: { 
+      js, 
+      cypress: pluginCypress,
+    }, 
+    extends: ["js/recommended", 
+      pluginCypress.configs.recommended,
+    ], 
+    languageOptions: { 
+      globals: globals.browser 
+    } 
+  },
   pluginVue.configs["flat/essential"],
 ]);
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import TheHeader from "./components/organisms/TheHeader.vue";
 </script>
 
