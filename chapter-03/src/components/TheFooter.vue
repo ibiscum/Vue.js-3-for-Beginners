81c6a8c42085a09d84a00ec232b878dedb107638
ibiscum <ulf.luecking@gmail.com>
2025-12-01 00:59:28 +0100

    ch03

---
+++ chapter-03/src/components/TheFooter.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <p>footer 2025</p>
+</template>
---
+++ chapter-03/src/components/molecules/SocialPost.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="SocialPost">
+    <div class="header">
+      <img
+        class="avatar"
+        :src="avatarSrc"
+      >
+      <div class="name">
+        {{ username }}
+      </div>
+      <div class="userId">
+        {{ userId }}
+      </div>
+    </div>
+    <div class="post">
+      {{ post }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+const props = defineProps({
+  username: { type: String, default: "John Doe" },
+  userId: { type: String, default: "user01" },
+  avatarSrc: { type: String, default: "https://i.pravatar.cc/40" },
+  post: { type: String, default: "message" },
+});
+onMounted(() => {
+  console.log(props.username);
+});
+</script>
+
+<style lang="scss">
+.SocialPost {
+  .header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+  }
+  .avatar {
+    border-radius: 50%;
+    margin-right: 12px;
+  }
+  .name {
+    font-weight: bold;
+    margin-right: 8px;
+    color: rgb(48, 10, 10);
+  }
+}
+</style>
