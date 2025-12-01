0e69f556030838dee191ec0ec686b1b30bc2471f
ibiscum <ulf.luecking@gmail.com>
2025-12-01 01:11:23 +0100

    ch04

---
+++ chapter-04/src/components/TheWelcome.vue
@@ -0,0 +1,40 @@
+<template>
+  <SocialPost
+    :username="posts[0].username"
+    :user-id="posts[0].userId"
+    :avatar-src="posts[0].avatar"
+    :post="posts[0].post"
+    :comments="posts[0].comments"
+  />
+  <SocialPost
+    :username="posts[1].username"
+    :user-id="posts[1].userId"
+    :avatar-src="posts[1].avatar"
+    :post="posts[1].post"
+    :comments="posts[1].comments"
+  />
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import SocialPost from "./molecules/SocialPost.vue";
+
+const posts = reactive([
+  {
+    username: "Username one",
+    userId: "usernameId1",
+    avatar: "https://i.pravatar.cc/40",
+    post: "This is my post",
+    comments: ["great post", "amazing post"],
+    tags: ["tag 1"],
+  },
+  {
+    username: "Username two",
+    userId: "usernameId2",
+    avatar: "https://i.pravatar.cc/40",
+    post: "This is my second post",
+    comments: [],
+    tags: ["tag 1", "tag 2"],
+  },
+]);
+</script>
