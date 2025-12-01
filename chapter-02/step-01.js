f01655340bfc2170f8048fbb961f92c119d5351d
ibiscum <ulf.luecking@gmail.com>
2025-12-01 19:17:00 +0100

    ch02

---
+++ chapter-02/step-01.js
@@ -0,0 +1,5 @@
+let firstName = "Simone";
+let secondName = "Cuomo";
+const fullName = `${firstName} ${secondName}`;
+console.log(fullName);
+// output: Simone Cuomo
---
+++ chapter-02/step-02.js
@@ -0,0 +1,8 @@
+let firstName = "Simone";
+let secondName = "Cuomo";
+const fullName = `${firstName} ${secondName}`;
+console.log(fullName);
+// output: Simone Cuomo
+firstName = "John";
+console.log(fullName);
+// output: Simone Cuomo
