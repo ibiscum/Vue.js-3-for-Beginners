05d61121a9f020daf13f52928f14279e03fcaafc
ibiscum <ulf.luecking@gmail.com>
2025-12-01 01:20:20 +0100

    ch04

--- chapter-04/src/components/HelloWorld.vue
+++ chapter-04/src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
       {{ msg }}
     </h1>
     <h3>
-      You’ve successfully created a project with
+      Hello Vitest
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
